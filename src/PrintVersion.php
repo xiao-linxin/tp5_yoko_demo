dfb787be4ac7268d0b2a3b4e31d9ffe4ecaa5a42
xiao_linxin <616916526@qq.com>
2018-09-29 16:51:27 +0800

    :leaves:

---
+++ src/PrintVersion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yoko;
+
+use think\App;
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+
+class PrintVersion extends Command
+{
+    public function configure() {
+        $this->setName('version')->setDescription('it will print current thinkphp5 version.');
+    }
+
+    public function execute(Input $input, Output $output) {
+        $output->write('current thinkphp5 version is:' . App::VERSION);
+    }
+}
